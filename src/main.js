3c4a33a80c9f7fbc7c2376ed0a4123165a2d2068
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-02 08:48:05 -0400

    delete view toggle

--- src/main.js
+++ src/main.js
@@ -7,22 +7,3 @@ if (navToggle && mainNav) {
     mainNav.classList.toggle("visible");
   });
 }
-
-// View Toggle
-const viewToggle = document.querySelector(".view-toggle");
-const main = document.querySelector("main");
-
-if (viewToggle && main) {
-  viewToggle.addEventListener("click", () => {
-    main.classList.toggle("grid");
-  });
-}
-
-// Search
-const searchInput = document.querySelector("#searchInput");
-const output = document.querySelector("#output");
-if (searchInput) {
-  searchInput.addEventListener("input", (e) => {
-    output.textContent = e.target.value;
-  });
-}
