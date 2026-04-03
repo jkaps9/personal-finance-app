e309295a04fbf2916ae90a747c08adce8565f61b
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-03 00:49:30 -0400

    minimize button toggles minimize class

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+const minimizeButton = document.querySelector("#minimize-btn");
+const header = document.querySelector("header");
 
+if (minimizeButton) {
+  minimizeButton.addEventListener("click", (e) => {
+    if (header) {
+      header.classList.toggle("minimized");
+    }
+  });
+}
